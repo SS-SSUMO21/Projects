7b678823e34005ebd66119fa465160cb7c5b828b
SS-SSUMO21 <samar.venthan01@gmail.com>
2026-05-18 22:15:17 -0400

    toggle chnaged for mobile

--- Temp-Email-Generator/frontend/src/app/layout.js
+++ Temp-Email-Generator/frontend/src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased">
       <body className="min-h-full flex flex-col">
-        <div className="fixed right-5 top-5 z-30 sm:right-8 sm:top-8">
+        <div className="theme-toggle-shell">
           <ThemeToggle />
         </div>
         {children}
